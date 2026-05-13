a4513ad1e6b190d1c16835da45ebf844e5a2b18c
Md Shariful Islam Rayhan <64407279+srayhan706@users.noreply.github.com>
2026-05-13 23:20:26 +0600

    Add: median-of-two-sorted-arrays - submission-0

---
+++ Data Structures & Algorithms/median-of-two-sorted-arrays/submission-0.cpp
@@ -0,0 +1,47 @@
+class Solution {
+public:
+    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+        // Ensure nums1 is smaller
+        if (nums1.size() > nums2.size()) {
+            return findMedianSortedArrays(nums2, nums1);
+        }
+
+        int m = nums1.size();
+        int n = nums2.size();
+
+        int left = 0, right = m;
+
+        while (left <= right) {
+            int i = (left + right) / 2;
+            int j = (m + n + 1) / 2 - i;
+
+            int Aleft  = (i == 0) ? INT_MIN : nums1[i - 1];
+            int Aright = (i == m) ? INT_MAX : nums1[i];
+
+            int Bleft  = (j == 0) ? INT_MIN : nums2[j - 1];
+            int Bright = (j == n) ? INT_MAX : nums2[j];
+
+            // correct partition
+            if (Aleft <= Bright && Bleft <= Aright) {
+
+                // even length
+                if ((m + n) % 2 == 0) {
+                    return (max(Aleft, Bleft) + min(Aright, Bright)) / 2.0;
+                }
+                // odd length
+                else {
+                    return max(Aleft, Bleft);
+                }
+            }
+
+            // move partition
+            else if (Aleft > Bright) {
+                right = i - 1;
+            } else {
+                left = i + 1;
+            }
+        }
+
+        return 0.0;
+    }
+};
